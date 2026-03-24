70eacc3a4775bc4136697ffbefc7c58789961721
nextnove <nextnove.com@gmail.com>
2026-03-24 10:01:49 +0900

    commit

--- internal/config/constants.go
+++ internal/config/constants.go
@@ -3,7 +3,7 @@ package config
 const (
 	// AppName is the user-facing name of the application
 	AppName = "PassVaultX"
-	AppVersion = "v1.0.2"
+	AppVersion = "v1.0.5"
 
 	// AppID is used for internal storage paths to avoid collisions with other apps
 	AppID = "com.nextnove.passvaultx"
